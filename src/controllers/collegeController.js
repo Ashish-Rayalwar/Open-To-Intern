fc30fd8f1cdd6326bda4408c68b701ee39b95dd2
Ashish Rayalwar <rayalwar.ashish.1@gmail.com>
2023-01-09 22:39:50 +0530

    Project-2 Complete

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,4 +1,4 @@
-const express = require("express");
+
 const collegeModel = require("../models/college");
 const internModel = require("../models/intern");
 
@@ -10,88 +10,66 @@ const createCollege = async (req, res) => {
   try {
     const data = req.body;
 
-    if (Object.keys(data).length == 0)
-      return res.status(400).send("Please send data in the Body");
-
+    if (Object.keys(data).length == 0)  return res.status(400).send({status:false,msg:"Please send data in the Body"});
+     
     const { name, fullName, logoLink } = data;
 
-    if (!name || !validate.test(name)) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "Please provide valid Name" });
-    }
+    if (!name || !validate.test(name)) return res.status(400).send({ status: false, msg: "Please provide valid Name" });
 
     let checkName = await collegeModel.findOne({ name: name });
 
-    if (checkName) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "College Name already exist" });
-    }
+    if (checkName) return res.status(400).send({ status: false, msg: "College Name already exist" });
 
-    console.log("checkname", checkName);
-
-    if (!fullName || !validate.test(fullName)) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "Please provide valid full Name" });
-    }
-
+    if (!fullName || !validate.test(fullName)) return res.status(400).send({ status: false, msg: "Please provide valid full Name" });
+      
     let checkFullName = await collegeModel.findOne({ fullName: fullName });
 
-    if (checkFullName) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "College Name already in use" });
-    }
-
-    if (!logoLink || !urlValidate.test(logoLink)) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "Please provide valid url link" });
-    }
-
+    if (checkFullName) return res.status(400).send({ status: false, msg: "College Name already in use" });
+      
+    if (!logoLink || !urlValidate.test(logoLink)) return res.status(400).send({ status: false, msg: "Please provide valid url link" });
+      
     let createData = await collegeModel.create(data);
 
     return res.status(400).send({ status: true, data: createData });
+
   } catch (err) {
+
     console.log("Error in create College", err.message);
 
     res.status(500).send({ status: false, Error: err.message });
   }
 };
 
+
+
+
 const getCollege = async (req, res) => {
   try {
     let collegeName = req.query.collegeName;
 
-    if (Object.keys(req.query).length === 0)
-      return res.status(400).send({ status: false, msg: "Please send query" });
-
-    if (!collegeName)
-      return res.status(400).send({ msg: "plz enter valid filter" });
-
-    let findCollege = await collegeModel
-      .findOne({
-        name: collegeName,
-        isDeleted: false,
-      })
-      .select({ name: 1, fullName: 1, logoLink: 1, _id: 0 });
+    if (Object.keys(req.query).length === 0) return res.status(400).send({ status: false, msg: "Please send valid filter" });
+      
+    if (!collegeName) return res.status(400).send({ msg: "plz enter valid filter" });
+     
+    let findCollege = await collegeModel.findOne({name:collegeName,isDeleted:false}).select({ name: 1, fullName: 1, logoLink: 1, _id: 1 });
 
     if (!findCollege) return res.status(404).send({ msg: "college not found" });
 
-    let internsss = await internModel.find({ collegeId: findCollege._id });
+    let interns = await internModel.find({ collegeId:findCollege._id,isDeleted:false }).select({isDeleted:0,__v:0,collegeId:0});
 
     let newData = {
       name: findCollege.name,
       fullName: findCollege.fullName,
       logoLink: findCollege.logoLink,
-      interns: internsss,
+      interns: interns
     };
 
     return res.status(200).send({ result: newData });
+
   } catch (err) {
-    console.log(err);
+
+    console.log("error in get college", err.message);
+
     res.status(500).send({ status: false, Error: err.message });
   }
 };
